c6d774e8ed46eca9a66af07345153b2b86f75e63
pSN0W <08yaksh08@gmail.com>
2022-06-30 20:44:47 +0530

    Solved bug

--- src/lib/web/renderer.rs
+++ src/lib/web/renderer.rs
@@ -6,17 +6,19 @@ pub enum RenderError {
     Render(#[from] handlebars::RenderError)
 }
 
-pub struct Renderer<'a> (handlebars::HandleBars<'a>);
+pub struct Renderer<'a> (handlebars::Handlebars<'a>);
 
 impl<'a> Renderer<'a> {
+    // This function creates the registery that is used for rendering
     pub fn new(template_dir: std::path::PathBuf) -> Self {
-        let mut renderer = handlebars::HandleBars::new();
+        let mut renderer = handlebars::Handlebars::new();
         renderer
-            .register_tempelates_directory(".hbs",&template_dir)
+            .register_templates_directory(".hbs",&template_dir)
             .expect("Failed to register template");
         Self(renderer)
     }
 
+    // This function converts anything that is serializable to json
     fn convert_to_value<S> (serializable : &S) -> serde_json::Value
     where
         S : serde::Serialize + std::fmt::Debug
@@ -29,7 +31,10 @@ impl<'a> Renderer<'a> {
     where
         P : ctx::PageContext + serde::Serialize + std::fmt::Debug,
     {
+        // You get the json value
         let mut value = Self::convert_to_value(&context);
+        // You mutably borrow the object and then add required keys to it because 
+        // our context structs had limited entries so we can add some dynamically here
         if let Some(value) = value.as_object_mut() {
             value.insert("_errors".into(), errors.into());
             value.insert("_title".into(),context.title().into());
@@ -38,10 +43,12 @@ impl<'a> Renderer<'a> {
         self.do_render(context.template_path(),value)
     }
 
+    // Function to actuall render
     fn do_render(&self,path : &str,ctx : serde_json::Value) -> String {
         self.0.render(path,&ctx).expect("Error rendering tempelate")
     }
 
+    // Same function as render you just add aditional user data with some context
     pub fn render_with_data<P,D>
     (
         &self,
